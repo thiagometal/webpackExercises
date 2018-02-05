756e05952a7c28cf7cef055b1b776f8441e9aca2
thiagometal <thiagometal.fne@gmail.com>
2018-02-05 19:51:22 -0400

    adiciona loader para interpretar css

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './ex/index.js',
@@ -10,6 +11,9 @@ module.exports = {
         port: 8080,
         contentBase: './public'
     },
+    plugins: [
+        new ExtractTextPlugin('app.css') //app.css é o resultado do processamento
+    ],
     module: {
         loaders: [{
             test: /.js?$/,
@@ -19,6 +23,9 @@ module.exports = {
                 presets: ['es2015', 'react'],
                 plugins: ['transform-object-rest-spread']
             }
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract("style-loader", "css-loader") // npm i --save-dev extract-text-webpack-plugin@1.0.1 style-loader@0.13.1 css-loader@0.26
         }]
     }
 }
