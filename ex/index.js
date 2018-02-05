4e8e9abdf30ae48f7f09d4e2cce5b7672805e1b9
thiagometal <thiagometal.fne@gmail.com>
2018-02-05 16:38:09 -0400

    adiciona utilizacao de classes no js com ec do EX03

--- ex/index.js
+++ ex/index.js
@@ -1,1 +1,12 @@
-require('./duvidaCruel')
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome
+    }
+
+    toString() {
+        return `Pessoa: ${this.nome}`
+    }
+}
+
+const pessoa = new Pessoa('Guilerme')
+console.log(pessoa.toString())
