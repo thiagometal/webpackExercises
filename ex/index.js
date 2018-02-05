77235c821e3fa4aefb14ebb795f2a68930544d71
thiagometal <thiagometal.fne@gmail.com>
2018-02-05 18:35:33 -0400

    adiciona configuracao de babel para traducao dos modulos import e export

--- ex/index.js
+++ ex/index.js
@@ -1,12 +1,4 @@
-class Pessoa {
-    constructor(nome) {
-        this.nome = nome
-    }
-
-    toString() {
-        return `Pessoa: ${this.nome}`
-    }
-}
+import Pessoa from './pessoa.js'
 
 const pessoa = new Pessoa('Guilerme')
 console.log(pessoa.toString())
